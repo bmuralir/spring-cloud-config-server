18bf9ca2f4f3bb4ad389487616b940c69dfd24a0
Balamurali R <54613676+bmuralir@users.noreply.github.com>
2023-02-13 06:39:18 +0530

    initial commit

---
+++ src/test/java/com/bmuralir/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmuralir.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
